06fd6c0f66758a23b9b9a6605fa5aaa9342d0f4e
Laze <yysi8771@gmail.com>
2023-06-30 16:01:24 +0900

    Spring Security - 2023.06.30

---
+++ src/main/java/blog/laze/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package blog.laze.repository;
+
+import blog.laze.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
